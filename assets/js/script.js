11eb7e39d769a9eed8155d6d8ae132aae979d92e
Samuel Martins <samuelsilvaa431@gmail.com>
2023-11-17 20:46:21 -0300

    Configurando botão de fechar e abrir menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.menu-mobile');
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
+        document.querySelector('.icon-mobile').src = "assets/Imagens/abrir-icon-mobile.png";
     } else {
-        menuMobile.classList.add('open')
+        menuMobile.classList.add('open');
+        document.querySelector('.icon-mobile').src = "assets/Imagens/fechar-icon-mobile.png";
+
     }
 }
